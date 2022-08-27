ddd8d7eea960423e720cd51a9ad5820f91c4a778
lenkaB-0101 <96007039+lenkaB-0101@users.noreply.github.com>
2022-08-27 13:20:14 +0200

    add

--- models.py
+++ models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-class Subject(models.Model):
-    name = models.CharField(max_length=256)
-    abbrev = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"NAME: {self.name}, ABBREV: {self.abbrev}"
 
 class Teacher(models.Model):
     name = models.CharField(max_length=256)
@@ -13,8 +7,19 @@ class Teacher(models.Model):
     title = models.CharField(max_length=16, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    teacher = models.ForeignKey(Teacher, related_name="subjects", on_delete=models.CASCADE)
+
+
     class Meta:
       ordering = ['created']
 
     def __str__(self):
       return f"{self.name} {self.surname}"
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=256)
+    abbrev = models.CharField(max_length=64)
+
+ def __str__(self):
+    return f"NAME: {self.name}, ABBREV: {self.abbrev}"
